c02d286a619321ea9104921b8e5826fea95bc872
Savyonsh <s.savyon@gmail.com>
2020-11-05 21:15:46 +0200

    Massage

--- compiler/parser.ml
+++ compiler/parser.ml
@@ -8,7 +8,7 @@ let digit = PC.range '0' '9';;
 
 let boolean_val = PC.disj PC.char 't' PC.char 'f';;
 
-let make_paired nt_left nt_right nt=
+let make_paired nt_left nt_right nt =
  let nt = PC.caten nt_left nt in
  let nt = PC.pack nt(function(_, e) -> e) in 
  let nt = PC.caten nt nt_right in 
@@ -18,15 +18,18 @@ let make_paired nt_left nt_right nt=
 let make_spaced nt=
   make_paired nt_withspaces nt_withspaces nt;;
 
-let tok_lparen= make_spaced( PC.char '(');;
+let tok_lparen = make_spaced(PC.char '(');;
 
-let tok_rparen= make_spaced( PC.char ')');;
+let tok_rparen = make_spaced(PC.char ')');;
 
-let tok_dot= make_spaced( PC.char '.');;
+let tok_dot = make_spaced(PC.char '.');;
 
 
 let parseBool = PC.caten PC.char '#' boolean_val;;
 
 let parseBool_withspaces = make_spaced(parseBool);;
+
+
+(* Sharon *)
 
 
