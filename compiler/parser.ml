5324e0944d8592e521a7b4987c6637b6707409ec
Shimon Nagar <62647652+shimonna394@users.noreply.github.com>
2020-11-08 19:12:10 +0200

    bool, nil, symbol
    add parser for comments and spaces together,
    bool and nil works.
    started to work on symbols and chars.

--- compiler/parser.ml
+++ compiler/parser.ml
@@ -1,33 +1,97 @@
 #use "pc.ml";;
+#use "reader.ml";;
+open PC;;
+open Reader;;
+exception X_this_should_not_happen;;
+(* Shimi *)
+
+(* General functions *)
+
+let nt_withspaces= star(const (fun ch -> ch <= ' '));; 
+
+let make_paired nt_left nt_right nt =
+  let nt = PC.caten nt_left nt in
+  let nt = PC.pack nt(function(_, e) -> e) in 
+  let nt = PC.caten nt nt_right in 
+  let nt = PC.pack nt(function(e, _) -> e) in
+   nt;;
+ 
+ let make_spaced nt=
+   make_paired nt_withspaces nt_withspaces nt;;
+
+  let nt_comments = 
+    let nt_semicolon = PC.char ';' in
+    let nt_rest = star (const (fun ch -> (ch != (char_of_int 4)) && (ch != (char_of_int 10)))) in
+    let nt_comments = pack (caten nt_semicolon nt_rest) (fun (e1, e2) -> e1 :: e2) in
+    let ignore_spaces = make_spaced nt_comments in
+    PC.star(ignore_spaces);;
+    
+  let make_comments nt = 
+    make_paired nt_comments nt_comments nt;;
+    
+  let make_spaces_and_comments nt = 
+    make_comments (make_spaced(nt));;
+  
+  (* let nt_sexp_comments exp =
+    let nt_start = word_ci "#;" in
+    let nt = pack (caten nt_start all_exps) (fun (hash, exp) -> exp) in
+    let nt = pack (caten nt all_exps) (fun (exp1, exp2) -> exp2) in
+    (make_spaced_and_commented nt) exp;; *)
+  
 
 (* parse Atomics *)
 
-let nt_withspaces  = PC.star(PC.char ' ');;
-
 let digit = PC.range '0' '9';;
 
-(*let boolean_val = PC.disj PC.char 't' PC.char 'f';;*)
+let lower_case = PC.range 'a' 'z';;
 
-let make_paired nt_left nt_right nt =
- let nt = PC.caten nt_left nt in
- let nt = PC.pack nt(function(_, e) -> e) in 
- let nt = PC.caten nt nt_right in 
- let nt = PC.pack nt(function(e, _) -> e) in
-  nt;;
+let upper_case = PC.range 'A' 'Z';;
 
-let make_spaced nt=
-  make_paired nt_withspaces nt_withspaces nt;;
+let dot = PC.char '.';;
 
-let tok_lparen = make_spaced(PC.char '(');;
+let parse_pref = PC.char '#';;
 
-let tok_rparen = make_spaced(PC.char ')');;
+let parse_slash = PC.char '/';;
 
-let tok_dot = make_spaced(PC.char '.');;
+let nt_lparen = make_spaces_and_comments(PC.char '(');;
 
+let nt_rparen = make_spaces_and_comments(PC.char ')');;
 
-(*let parseBool = PC.caten PC.char '#' boolean_val;;*)
+(* parse booleans *)
 
-(*let parseBool_withspaces = make_spaced(parseBool);;*)
+let parse_booleans exp =
+  let nt = disj (word_ci "#f") (word_ci "#t") in
+  let nt_val  = pack nt (fun (prefix) -> match((list_to_string prefix)) with
+      | "#f" -> Bool(false)
+      | "#F" -> Bool(false)
+      | "#t" -> Bool(true)
+      | "#T" -> Bool(true)
+      | _ -> raise X_no_match) in
+  let nt_val = make_spaced nt_val in
+  (make_spaces_and_comments nt_val) exp;;
+
+(* parse nil *)
+
+let parse_nil =
+  let nt_nil = pack (caten nt_lparen nt_rparen) (fun (lparen, rparen) -> Nil) in
+  let nt_nil = make_spaces_and_comments nt_nil in
+  nt_nil;;
+
+(* parse symbol *)  
+
+(*let parse_symbol = 
+  let parse_SymbolCharNoDot = PC.disj_list[digit;lower_case;upper_case;(PC.char '!');
+  (PC.char '$');(PC.char '^');(PC.char '*');(PC.char '-');(PC.char '_');(PC.char '=');
+  (PC.char '+');(PC.char '<');(PC.char '>');(PC.char '?');(PC.char '/');(PC.char ':')] in
+  let parse_SymbolCharNoDot = PC.pack parse_SymbolCharNoDot (lowercase_ascii) in
+  let parse_symbolChar = PC.disj parse_SymbolCharNoDot dot in
+  let plus_symbol_char = PC.plus(parse_symbolChar) in
+  let parse_symbol = PC.disj parse_SymbolCharNoDot plus_symbol_char in
+  let parse_symbol = PC.pack (parse_symbol) (fun (prefix) ->  (Symbol(list_to_string prefix))) in
+  make_spaces_and_comments parse_symbol;; *)
+
+(* parse char *)
+
 
 
 (* Sharon *)
